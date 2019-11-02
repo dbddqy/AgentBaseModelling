192afbabaa970a565a9e778e6ecbfbf2352d865a
Yue <dbddqy@gmail.com>
2019-11-03 04:44:03 +0800

    second init

---
+++ AgentBasedModelling/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Student
+    {
+        public string name;
+        public int age;
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
