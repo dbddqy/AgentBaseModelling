b128fcdfdd0c0bc9ece5583c954ef642c8f79963
Yue <dbddqy@gmail.com>
2019-11-03 05:31:44 +0800

    first test

---
+++ AgentBasedModelling/MyAgent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Rhino.Geometry;
+using ICD.AbmFramework.Core.Agent;
+using ICD.AbmFramework.Core.Behavior;
+
+namespace AgentBasedModelling
+{
+    public class MyAgent : MeshFaceAgent
+    {
+        public int myDefaultValue;
+
+        public MyAgent(int faceIndex, double initialValue, List<BehaviorBase> behaviors) : base(faceIndex, initialValue, behaviors)
+        {
+            this.myDefaultValue = 1;
+        }
+
+
+    }
+}
